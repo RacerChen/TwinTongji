ef677a2f88d7871a539dfc31d194a84ac6a7b6b4
RacerChen <326158268@qq.com>
2020-12-24 18:58:07 +0800

    Update 20201224-1858

--- TwinTongji/TabVC/ProfileVC.h
+++ TwinTongji/TabVC/ProfileVC.h
@@ -6,8 +6,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import<SDCycleScrollView.h>
 
 @interface ProfileVC : UIViewController
 
+@property(strong, nonatomic) IBOutlet UIImageView *imgview_portrait;
+@property(strong, nonatomic) IBOutlet SDCycleScrollView *cycerView_photos;
+
+-(IBAction) btn_click_imgview_portrait;
 
 @end
--- TwinTongji/TabVC/ProfileVC.m
+++ TwinTongji/TabVC/ProfileVC.m
@@ -7,16 +7,45 @@
 
 #import "ProfileVC.h"
 
-@interface ProfileVC ()
+@interface ProfileVC () <SDCycleScrollViewDelegate>
 
 @end
 
 @implementation ProfileVC
 
+@synthesize imgview_portrait;
+@synthesize cycerView_photos;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+//    [self init_cycleScrollView];
+    [self init_portrait:@"portrait.jpg"];
 }
 
+-(IBAction) btn_click_imgview_portrait
+{
+    NSLog(@"Click Portrait");
+}
+
+-(void) init_cycleScrollView
+{
+    NSArray *img_arr = [NSArray arrayWithObjects:[UIImage imageNamed:@"scroll_img_1.jpg"], [UIImage imageNamed:@"scroll_img_2.jpg"], [UIImage imageNamed:@"scroll_img_3.jpg"], nil];
+    self.cycerView_photos = [SDCycleScrollView cycleScrollViewWithFrame:CGRectZero
+                                                                delegate:self
+                                                        placeholderImage:[UIImage imageNamed:@"tabbar_icon0_normal"]];
+            
+    self.cycerView_photos.localizationImageNamesGroup = img_arr;
+    self.cycerView_photos.autoScrollTimeInterval = 5.;// 自动滚动时间间隔
+    self.cycerView_photos.pageControlAliment = SDCycleScrollViewPageContolAlimentRight;// 翻页 右下角
+    self.cycerView_photos.titleLabelBackgroundColor = [UIColor clearColor];// 图片对应的标题的 背景色。（因为没有设标题）
+}
+
+-(void) init_portrait: (NSString *) imageName
+{
+    imgview_portrait.image = [UIImage imageNamed:imageName];
+    imgview_portrait.layer.cornerRadius = imgview_portrait.frame.size.height / 5;
+}
 
 @end
+
