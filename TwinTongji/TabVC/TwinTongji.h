714482a339df4ca84c5c4a54cccac1bfe44da5e4
RacerChen <326158268@qq.com>
2021-01-07 13:49:01 +0800

    Segment View Finished

--- TwinTongji/TabVC/TwinTongji.h
+++ TwinTongji/TabVC/TwinTongji.h
@@ -9,5 +9,11 @@
 
 @interface TwinTongji : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIView *first_containView;
+@property (strong, nonatomic) IBOutlet UIView *second_containView;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *segment_controll;
+
+
+- (IBAction)click_seg_controll:(id)sender;
 
 @end
--- TwinTongji/TabVC/TwinTongji.m
+++ TwinTongji/TabVC/TwinTongji.m
@@ -19,16 +19,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    ///地图需要v4.5.0及以上版本才必须要打开此选项（v4.5.0以下版本，需要手动配置info.plist）
-    [AMapServices sharedServices].enableHTTPS = YES;
-        
-    ///初始化地图
-    MAMapView *_mapView = [[MAMapView alloc] initWithFrame:self.view.bounds];
-    _mapView.showsIndoorMap = YES;
-        
-    ///把地图添加至view
-    [self.view addSubview:_mapView];
+//    ///地图需要v4.5.0及以上版本才必须要打开此选项（v4.5.0以下版本，需要手动配置info.plist）
+//    [AMapServices sharedServices].enableHTTPS = YES;
+//
+//    ///初始化地图
+//    MAMapView *_mapView = [[MAMapView alloc] initWithFrame:self.view.bounds];
+//    _mapView.showsIndoorMap = YES;
+//
+//    ///把地图添加至view
+//    [self.view addSubview:_mapView];
 }
 
 
+- (IBAction)click_seg_controll:(id)sender
+{
+    switch (self.segment_controll.selectedSegmentIndex)
+    {
+        case 0:
+            self.first_containView.hidden = false;
+            self.second_containView.hidden = true;
+            break;
+            
+        case 1:
+            self.first_containView.hidden = true;
+            self.second_containView.hidden = false;
+            break;
+            
+        default:
+            break;
+    }
+}
 @end
